65a3e55d36ce7fba8a657828ca639c57e5f0254a
makochang <metal.shinoda@gmail.com>
2022-11-17 19:10:47 +0900

    ファイルを上書きモードに変更

--- Assets/Scripts/FileManager.cs
+++ Assets/Scripts/FileManager.cs
@@ -2,18 +2,28 @@ using System.IO;
 
 public static class FileManager
 {
+    static string initData = "";
+
+    /// <summary>
+    /// 指定のファイルパスに文字列を保存する
+    /// </summary>
+    /// <param name="path">ファイルパス</param>
+    /// <param name="data">書き込む文字列</param>
     public static void WriteString(string path, string data)
     {
-        StreamWriter writer = new StreamWriter(path, true);
-        writer.WriteLine(data);
+        StreamWriter writer = new StreamWriter(path,false);
+        writer.Write(data);
         writer.Close();
-        StreamReader reader = new StreamReader(path);
-        reader.ReadToEnd();
-        reader.Close();
     }
 
+    /// <summary>
+    /// 指定のファイルパスから文字列を読み取り、返す
+    /// </summary>
+    /// <param name="path">ファイルパス</param>
+    /// <returns>読み込んだ文字列</returns>
     public static string ReadString(string path)
     {
+        //ファイルの有無で処理を変更する
         if (File.Exists(path))
         {
             StreamReader reader = new StreamReader(path);
@@ -24,7 +34,9 @@ public static class FileManager
         }
         else
         {
-            return "0";
+            //ファイルが存在しなければ、セーブデータを初期化する
+            WriteString(path, initData);
+            return initData;
         }
     }
 }
